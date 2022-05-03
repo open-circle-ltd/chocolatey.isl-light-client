c1e56cab7362d535bfd5197108453f4b24de8f16
bmcatherasoo <benaiahcatherasoo@gmail.com>
2022-05-03 13:08:29 +0200

    Final Version

--- package/tools/chocolateyuninstall.ps1
+++ package/tools/chocolateyuninstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-(Get-Content -Raw -Path "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\path.json" | ConvertFrom-Json).PSObject.Properties | ForEach-Object {
+(Get-Content -Raw -Path "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\desktopshortcut.json" | ConvertFrom-Json).PSObject.Properties | ForEach-Object {
     Remove-Item "$($_.Value)"
 }
